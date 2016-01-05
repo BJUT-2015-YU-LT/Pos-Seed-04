4cbfea0af6c69711311ef1329c2647840e856897
zhouxiyue <15810683906@163.com>
2016-01-05 20:22:55 +0800

    pos

---
+++ 需求二/GOODS.java
@@ -0,0 +1,11 @@
+/**
+ * Created by cyd on 2016/1/5.
+ */
+public class GOODS {
+    public String barcode;
+    public String name;
+    public String unit;
+    public double price;
+    public double discount;
+    public int num=1;
+}
